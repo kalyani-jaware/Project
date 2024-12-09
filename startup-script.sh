a7eb3e5ab9e60f25e4e997643d9d7459c80499e1
kalyani-jaware <145304548+kalyani-jaware@users.noreply.github.com>
2024-12-09 08:43:57 -0700

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -14,8 +14,17 @@ cd mobilenet_project
 # Install Python dependencies
 pip3 install -r requirements.txt
 
-# Set up the service account key for authentication (if needed inside the app)
-export GOOGLE_APPLICATION_CREDENTIALS="/lab-5-437901-4cd443d66c01.json"
+# Fetch VM's external IP for SCP
+EXTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+
+# Copy the service account JSON (adjust for pre-upload location)
+scp /home/kaja7104/Project/lab-5-437901-1c5f9e8d39f4.json "kaja7104_colorado_edu@${EXTERNAL_IP}:/home/kaja7104_colorado_edu/final_project/"
+
+# Set up the service account key for authentication
+export GOOGLE_APPLICATION_CREDENTIALS="/home/kaja7104_colorado_edu/final_project/lab-5-437901-1c5f9e8d39f4.json"
+
+# # Set up the service account key for authentication (if needed inside the app)
+# export GOOGLE_APPLICATION_CREDENTIALS="/lab-5-437901-4cd443d66c01.json"
 
 # Copy the service account key (pre-uploaded to VM metadata)
 if [ -f "/tmp/lab-5-437901-4cd443d66c01.json" ]; then
