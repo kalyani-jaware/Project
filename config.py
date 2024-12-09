600fb0a3e7b0b2f30250afba0e65419cd41beb91
kalyani-jaware <145304548+kalyani-jaware@users.noreply.github.com>
2024-12-08 21:36:03 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Google Cloud configuration
+GOOGLE_CLOUD_BUCKET_NAME = "airflow-project-storage"
+
+# Database configuration
+DATABASE_URI = "sqlite:///hyperparameters.db"  # SQLite for local DB, or use PostgreSQL, MySQL
